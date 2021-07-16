70d922d0df3d3b95fa933c56dcdd891239cdea5e
Marko <mare.sampbn@gmail.com>
2021-07-16 15:11:02 +0200

    tailwind

---
+++ src/themes/tailwindcss.js
@@ -0,0 +1,12 @@
+module.exports = {
+    nav:'',
+    count:'',
+    wrapper:'',
+    list:'pagination',
+    item:'page-item',
+    link:'page-link',
+    next:'',
+    prev:'',
+    active:'bg-indigo-50 border-indigo-500 text-indigo-600',
+    disabled:'disabled'
+}
